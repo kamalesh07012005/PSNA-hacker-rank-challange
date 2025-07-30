469971e4f8e545ce30bb723de4291cd1b4a4bd6e
kamalesh07012005 <kamalesh07012005@gmail.com>
2025-07-30 20:56:27 +0530

    Create case.java

---
+++ case.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+     Scanner sc=new Scanner(System.in);
+    char a;
+    a=sc.nextLine().charAt(0);
+    if(a>='A'&&a<='Z')
+        System.out.print("UPPERCASE");
+    else
+        System.out.print("LOWERCASE");
+    
+    }
+}
