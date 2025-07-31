0cb1dda141d6e274aa83bae027539c41329f5db8
kamalesh <kamalesh07012005@gmail.com>
2025-07-31 21:12:39 +0530

    Create positivenegative.java

---
+++ positivenegative.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int a;
+    a=sc.nextInt();
+    if(a>0)
+        System.out.print("Positive");
+    
+    else if(a<0)
+    
+        System.out.print("Negative");
+        
+    else
+        System.out.print("Zero");
+    
+    
+    }
+}
+       
+
