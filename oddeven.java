a0400beadccef8a28b464249f97cca92ba05e3b9
kamalesh <kamalesh07012005@gmail.com>
2025-07-31 21:11:34 +0530

    Create oddeven.java

---
+++ oddeven.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int a;
+    a=sc.nextInt();
+    if(a%2==0){
+        System.out.print("EVEN");
+    }
+    else{
+        System.out.print("ODD");
+    }
+        
+        
+    }
+}
