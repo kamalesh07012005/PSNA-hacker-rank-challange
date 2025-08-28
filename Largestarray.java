d63d8521a83f2b959fd6387cc7ad682c3324a445
kamalesh <kamalesh07012005@gmail.com>
2025-08-28 23:02:34 +0530

    Create Largestarray.java

---
+++ Largestarray.java
@@ -0,0 +1,24 @@
+mport java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        int max = arr[0];
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        System.out.println(max);
+    }
+}
