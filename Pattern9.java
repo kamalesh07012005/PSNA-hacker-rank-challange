73133238e3fff761671e2f7fbbfd693f77fe8369
kamalesh <kamalesh07012005@gmail.com>
2025-08-29 07:36:28 +0530

    Create Pattern9.java

---
+++ Pattern9.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+       int n,row,col;
+        n=sc.nextInt();
+        int size=2*n-1;
+        for(row=1;row<=size;row++){
+            for(col=1;col<=size;col++){
+               if(col+row==size+1 || col==row ){
+                System.out.print("*");
+            }
+            else{
+                System.out.print(" ");    
+            }
+            }
+            System.out.println();
+        }
+        
+    }
+}
